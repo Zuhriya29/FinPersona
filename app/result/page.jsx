18239763d6f7e42aae7bd1953cf59b15c4cfc480
Zuhriya29 <113580042+Zuhriya29@users.noreply.github.com>
2026-04-28 09:18:09 +0700

    edit result

--- app/result/page.jsx
+++ app/result/page.jsx
@@ -46,8 +46,8 @@ function calculateScore(data) {
 }
 
 function getInvestorType(score) {
-    if (score <= 6) return "Conservative";
-    if (score <= 10) return "Moderate";
+    if (score <= 8) return "Conservative";
+    if (score <= 13) return "Moderate";
     return "Aggressive";
 }
 
@@ -174,7 +174,7 @@ LEARNING:
 
                 <div className="result-card risk-card">
                     <h3 className="font-bold text-(--primary-color) text-xl mb-2">Risk Tolerance</h3>
-                    <p className="text-sm text-(--second-color) -500">{result.riskMeter}/17</p>
+                    <p className="text-sm text-(--second-color)-500">{result.riskMeter}/17</p>
                     <div className="risk-meter-container">
                         <div className="risk-bar"></div>
                         <div className="risk-pointer" style={{ left: `${riskScore}%` }}></div>
